7ea3233a13c8cead85054c41ab869c9e2289a55a
14vosx <14vosx@gmail.com>
2026-02-21 19:30:18 -0300

    fix: reset project - 04

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import { buildServer } from "./src/server.js";
 
 const port = Number(process.env.PORT || 3000);
-const host = "0.0.0.0";
+const host = "::"; // em vez de "0.0.0.0"
 
 const server = await buildServer();
 
 try {
-	await server.listen({ port, host });
-	console.log(`[hsc-auth] listening on http://${host}:${port}`);
+	app.listen(port, host, () => {
+    console.log(`[hsc-auth] listening on http://[${host}]:${port}`);
+  });
 } catch (err) {
 	console.error(err);
 	process.exit(1);
